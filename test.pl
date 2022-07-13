5884b714cfe8d2427c167dab8d4fbd76351bccce
philip r brenan <philiprbrenan@gmail.com>
2022-07-13 09:00:08 +0100

    Add files via upload

---
+++ test.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -I/home/phil/perl/cpan/NasmX86/lib
+#-------------------------------------------------------------------------------
+# Test Nasm:X86
+# Philip R Brenan at gmail dot com, Appa Apps Ltd Inc, 2021-2022
+#-------------------------------------------------------------------------------
+use warnings FATAL => qw(all);
+use strict;
+use Nasm::X86 qw(:all);
+use utf8;
+use Test::More;
+
+Mov rax, 1;
+PrintOutRegisterInHex rax;
+ok Assemble eq => <<END;
+   rax: .... .... .... ...1
+END
+
+done_testing;
